36c2724e535163ff7336ab4f2f2f992106edae44
D.Zaharov <tdimdimich@gmail.com>
2020-10-21 20:04:03 +0300

    webpack lib import order

--- web/scripts/browser/index.js
+++ web/scripts/browser/index.js
@@ -1,9 +1,7 @@
-
-
 ;(async () => {
+	const createApp = (await import('containers/App/create')).default
 	const React = (await import('react')).default
 	const ReactDom = (await import('react-dom')).default
-	const createApp = (await import('containers/App/create')).default
 	const App = await createApp()
 	ReactDom.render(React.createElement(App), document.getElementById('app'))
 })()
